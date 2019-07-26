812dd2e7c4a63be5e7b95c140642d9a04c53d275
Cesar <cesarsaa1991@gmail.com>
2019-07-26 16:44:32 -0500

    integracion parcial del templade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
 @Component({
+  
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ProyectoTickets';
+  public hola : string = "hola mundo desde Angular :)";
+  public prueba : string = "Otro texto de prueba";
+  public name : string = "Nombre Generico 1";
+  public edad : number = 29;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { TicketsComponent } from './tickets/tickets.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
